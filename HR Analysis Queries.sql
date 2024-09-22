03a64f6964c308507791bd313786ff1cc2599dbe
CHow4 <CHRIS.M.HOWARD3@Gmail.com>
2024-09-22 15:46:26 -0400

    Update HR Analysis Queries.sql

--- HR Analysis Queries.sql
+++ HR Analysis Queries.sql
@@ -19,7 +19,7 @@ SELECT
 FROM `astral-pursuit-390317.hr_data.human_resources`
 WHERE termdate IS NULL OR termdate > CURRENT_DATE;
 
-
+--Creating age group buckets
 SELECT 
   CASE 
     WHEN age >=22 AND age <=25 THEN '22-25'
@@ -37,3 +37,66 @@ WHERE termdate IS NULL OR termdate > CURRENT_DATE
 GROUP BY age_group
 ORDER BY age_group
 
+--4. How many employees work at headquarters versus remote locations?
+SELECT location, COUNT(*) AS Total
+FROM `astral-pursuit-390317.hr_data.human_resources`
+WHERE termdate IS NULL OR termdate > CURRENT_DATE
+GROUP BY location
+ORDER BY 2 DESC
+
+--5. How does the gender distribution vary across departments and job titles?
+SELECT department, jobtitle, gender, COUNT(*) AS Total
+FROM `astral-pursuit-390317.hr_data.human_resources`
+WHERE termdate IS NULL OR termdate > CURRENT_DATE
+GROUP BY department, jobtitle, gender
+ORDER BY 1, 4 DESC
+
+--6. What is the distribution of job titles across the company?
+SELECT jobtitle, COUNT(*) AS Total
+FROM `astral-pursuit-390317.hr_data.human_resources`
+WHERE termdate IS NULL OR termdate > CURRENT_DATE
+GROUP BY jobtitle
+ORDER BY 2 DESC
+
+--7. What is the distribution of employees across locations by state?
+SELECT location_state, COUNT(*) AS Total
+FROM `astral-pursuit-390317.hr_data.human_resources`
+WHERE termdate IS NULL OR termdate > CURRENT_DATE
+GROUP BY location_state
+ORDER BY 2 DESC
+  
+--8. What is the average length of employment for employees who have been terminated?
+SELECT ROUND(AVG(DATE_DIFF(termdate, hire_date, YEAR))) AS avg_employment_length_years
+FROM `astral-pursuit-390317.hr_data.human_resources`
+WHERE termdate IS NOT NULL AND termdate < CURRENT_DATE
+
+--9. Which department has the highest turnover rate?
+SELECT department, total_count, terminated_count, ROUND((terminated_count/total_count)*100,2) AS Turnover_rate
+FROM 
+(
+  SELECT department, COUNT(*) AS total_count,
+  SUM (CASE 
+    WHEN termdate IS NOT NULL AND termdate < CURRENT_DATE THEN 1 
+    ELSE 0 
+    END) AS terminated_count
+  FROM `astral-pursuit-390317.hr_data.human_resources`
+  GROUP BY department
+) AS termed_by_dept
+ORDER BY turnover_rate DESC
+
+--10. How has the company's employee count changed over time based on hire and term dates?
+SELECT year, hires, terminations, 
+    hires - terminations AS net_change, 
+    ROUND((hires - terminations)/hires*100,2) AS net_change_percent
+FROM(
+SELECT 
+  EXTRACT(YEAR FROM hire_date) AS year,
+  COUNT(*) AS hires,
+  SUM(CASE WHEN termdate IS NOT NULL AND termdate < CURRENT_DATE THEN 1
+  ELSE 0 END) AS terminations
+FROM `astral-pursuit-390317.hr_data.human_resources`
+GROUP BY year
+) AS e1
+ORDER BY year 
+
+--11. What is the tenure distribution for each department?
